3fe1cf21b290e87ac98ad8f8afca4e4700cb41fe
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2021-11-16 08:15:04 +0100

    Add project files.

---
+++ GuessGame_IT1A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuessGame_IT1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
